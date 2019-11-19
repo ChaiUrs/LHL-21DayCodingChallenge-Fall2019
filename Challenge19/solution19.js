38f852ace7dde7224a26c1eec11b12a9421dbc45
ChaiUrs <mysorechaitra@gmail.com>
2019-11-19 21:57:39 +0000

    Updated solution to challenge 19

--- Challenge19/solution19.js
+++ Challenge19/solution19.js
@@ -19,11 +19,15 @@ const pumpkinSpice = money => {
   const pieSlices = Math.floor(money / 5);
   let remainingMoney = money % 5;
   pumpkinSpiceinGrams += pieSlices * 30;
-  console.log("Pumpkin Pie Slices the festival-goer can buy:", pieSlices);
+  console.log(
+    "Pumpkin Pie Slices the festival-goer can buy:",
+    `${pieSlices} slice`
+  );
   console.log(
     "Money left after buying Pumpkin Pie Slices:",
     `$${remainingMoney}`
   );
+  console.log("Pumpkin Spice Consumed :", `${pumpkinSpiceinGrams} grams`);
   console.log(
     "-----------------------------------------------------------------"
   );
@@ -31,11 +35,15 @@ const pumpkinSpice = money => {
   const pieLattes = Math.floor(remainingMoney / 3);
   remainingMoney = remainingMoney % 3;
   pumpkinSpiceinGrams += pieLattes * 15;
-  console.log("Pumpkin Spice Lattes the festival-goer can buy:", pieLattes);
+  console.log(
+    "Pumpkin Spice Lattes the festival-goer can buy:",
+    `${pieLattes} latte`
+  );
   console.log(
     "Money left after buying Pumpkin Pie Spices & Lattes:",
     `$${remainingMoney}`
   );
+  console.log("Pumpkin Spice Consumed :", `${pumpkinSpiceinGrams} grams`);
   console.log(
     "-----------------------------------------------------------------"
   );
@@ -43,18 +51,20 @@ const pumpkinSpice = money => {
   const pieMacarons = Math.floor(remainingMoney / 1);
   remainingMoney = remainingMoney % 1;
   pumpkinSpiceinGrams += pieMacarons * 3;
-  console.log("Pumpkin Spice Macarons the festival-goer can buy:", pieMacarons);
   console.log(
-    "Money left after buying Pumpkin Pie Slices,Lattes & Macarons:",
+    "Pumpkin Spice Macarons the festival-goer can buy:",
+    `${pieMacarons} macaron`
+  );
+  console.log(
+    "Money left after buying Pumpkin Pie Slices, Lattes & Macarons:",
     `$${remainingMoney}`
   );
   console.log(
     "-----------------------------------------------------------------"
   );
-
   console.log(
     "Total Grams of Pumpkin Spice the festival-goer is buying:",
-    `${pumpkinSpiceinGrams}grams`
+    `${pumpkinSpiceinGrams} grams`
   );
   console.log(
     "-----------------------------------------------------------------"
